e2ea0be2de98af94f44b800763b68fb6c31120dc
MHarismemon <harismemon2k47@gmail.com>
2024-10-09 01:59:48 +0500

    Update version 1 beta (small fixes)

--- backend/models/StudentOldAcademicInfo.js
+++ backend/models/StudentOldAcademicInfo.js
@@ -1,34 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const studentOldAcademicInfoSchema = new Schema({
-    studentId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Student',
-        required: true 
+const studentOldAcademicInfoSchema = new Schema(
+  {
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
     },
-    instituteName: { 
-        type: String, 
-        required: [true, 'Name of the institute is required.'] 
+    instituteName: {
+      type: String,
+      // required: [true, 'Name of the institute is required.]
+      default: "-",
     },
-    location: { 
-        type: String, 
-        required: [true, 'Location is required.'] 
+    location: {
+      type: String,
+      default: "-",
+      // required: [true, 'Location is required.']
     },
-    from: { 
-        type: Date, 
-        required: [true, 'From date is required.'] 
+    from: {
+      type: Date,
+      default: null,
+      // required: [true, 'From date is required.']
     },
-    to: { 
-        type: Date, 
-        required: [true, 'To date is required.'] 
+    to: {
+      type: Date,
+      default: null,
+      // required: [true, 'To date is required.']
     },
-    upToClass: { 
-        type: String, 
-        required: [true, 'Up to Class is required.'] 
-    }
-}, {
+    upToClass: {
+      type: String,
+      default: "-",
+      //required: [true, 'Up to Class is required.']
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-module.exports = mongoose.model('studentOldAcademicInfo', studentOldAcademicInfoSchema);
+module.exports = mongoose.model(
+  "studentOldAcademicInfo",
+  studentOldAcademicInfoSchema
+);
